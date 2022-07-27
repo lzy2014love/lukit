d718bfeba35876d1df4a9fb2babab872197e75db
lzy2014love <lzy2014love@outlook.com>
2022-07-27 20:00:50 +0800

    release 1.0.2

--- packages/util/src/environment.ts
+++ packages/util/src/environment.ts
@@ -8,21 +8,21 @@ export const isNode = typeof process !== 'undefined' && typeof global !== 'undef
 
 export const ua = isBrowser ? window.navigator.userAgent.toLowerCase() : ''
 
-export const isIE = ua && /msie|trident/.test(ua)
+export const isIE = ua ? /msie|trident/.test(ua) : false
 
-export const isIE9 = ua && ua.includes('msie 9.0')
+export const isIE9 = ua ? ua.includes('msie 9.0') : false
 
-export const isEdge = ua && /edge\/|Edg\//.test(ua)
+export const isEdge = ua ? /edge\/|Edg\//.test(ua) : false
 
-export const isAndroid = ua && ua.includes('android')
+export const isAndroid = ua ? ua.includes('android') : false
 
-export const isIOS = ua && /iphone|ipad|ipod|ios/.test(ua)
+export const isIOS = ua ? /iphone|ipad|ipod|ios/.test(ua) : false
 
-export const isChrome = ua && /chrome\/\d+/.test(ua) && !isEdge
+export const isChrome = ua ? /chrome\/\d+/.test(ua) && !isEdge : false
 
-export const isPhantomJS = ua && /phantomjs/.test(ua)
+export const isPhantomJS = ua ? /phantomjs/.test(ua) : false
 
-export const isFirefox = ua && ua.match(/firefox\/(\d+)/)
+export const isFirefox = ua ? ua.match(/firefox\/(\d+)/) : false
 
 export const isWindows = (ua && ua.includes('Windows')) || (isNode && process.platform === 'win32')
 
@@ -30,4 +30,15 @@ export const isMacintosh = (ua && ua.includes('Macintosh')) || (isNode && proces
 
 export const isLinux = (ua && ua.includes('Linux')) || (isNode && process.platform === 'linux')
 
-export const isElectron = typeof process?.versions?.electron === 'string'
+export const isElectronRendererProcess =
+  typeof window !== 'undefined' && typeof window.process === 'object' && (window.process as any).type === 'renderer'
+
+export const isElectronMainProcess =
+  typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron
+
+export const isElectronBrowser =
+  typeof navigator === 'object' &&
+  typeof navigator.userAgent === 'string' &&
+  navigator.userAgent.indexOf('Electron') >= 0
+
+export const isElectron = isElectronRendererProcess || isElectronMainProcess || isElectronBrowser
---
+++ samples/util-sample/src/main.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+import '@lukit/css'
+import { isElectron } from "@lukit/util";
+
+console.log("================");
+console.log("isElectron  >>>>>>", isElectron);
+console.log("================");
